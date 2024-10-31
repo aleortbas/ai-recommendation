e1af64fa81210ea323b4f3accb1c0986fb9455ed
aleortbas <aleortbas@gmail.com>
2024-10-30 21:55:04 -0500

    route aboute creted and skeleton home page

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function page() {
+    return(
+        <><p className="mt-20">Customers page</p></>    
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,24 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 
 import Link from 'next/link';
+import { BsNewspaper } from 'react-icons/bs';
+import { HiMusicalNote } from 'react-icons/hi2';
+import { IoMdAdd } from 'react-icons/io';
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span>
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-        </div>
-      </div>
-    </main>
+    <>
+    <div className='grid justify-items-center text-white bg-inherit'>
+      <h1>Explore the Possibilities of</h1>
+      <h1>AI with<span className='text-[#fd7e14]'>FlowAI</span></h1>
+      <button className='bg-[#8F79FF] flex items-center justify-center rounded-md w-36 h-8'><IoMdAdd /> sapas</button>
+    </div> 
+    <div className='container grid grid-cols-3 mt-3 m-auto bg-white md:w-3/6'>
+      <div className='border-solid border-2 border-red-600 w-fit'><BsNewspaper /> sdfds</div>
+      <div className='border-solid border-2 border-red-600 w-fit'><HiMusicalNote /> sdfds</div>
+      <div className='border-solid border-2 border-red-600 w-fit'><HiMusicalNote /> sdfds</div>
+      <div className='border-solid border-2 border-red-600 w-fit'><HiMusicalNote /> sdfds</div>
+
+    </div>
+    </>
   );
 }
