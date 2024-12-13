48374e7aa1e71123dfa4c7f78c3c2606679a8418
Alejandro <aleortbas@gmail.com>
2024-12-12 21:45:53 -0500

    feature: consume api authorization endpoint and open tab

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,13 +1,35 @@
 "use client"
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
 
 export default function page() {
     const router = useRouter();
 
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        try {
+            const response = await fetch('http://localhost:5000/spotify')
+            console.log("response: ", response);
+            if (response.ok) {
+                const data = await response.json();
+                console.log("data: ", data.authorization);
+                window.open(data.authorization,'_blank')
+              } else {
+                const errorData = await response.json();
+                console.log("Error response:", response.status, errorData);
+              }
+        } catch (error) {
+            
+        }
+    }
+
     return(
         <div>
-            <h1>sapa perra</h1>
+            <form onSubmit={handleSubmit}>
+            <button type="submit" className="bg-red-500">Spotify</button>
+            </form>
         </div>
     )
 }
